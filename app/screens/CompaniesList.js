05a65cff14d1625baa6dbbcb827dc81e8e589e88
Mohamed Tarek <57064645+MohamedT858@users.noreply.github.com>
2024-05-08 17:07:07 +0300

    Update Home screen layout and add SafeAreaView component

--- app/screens/CompaniesList.js
+++ app/screens/CompaniesList.js
@@ -1,5 +1,5 @@
 import {
-	View, Text, Modal, ScrollView, FlatList, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard
+	View, Text, Modal, ScrollView, FlatList, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard, SafeAreaView
 } from "react-native";
 import React, { useEffect, useState, useRef } from "react";
 import companiesList from "../data/busRoutes.json"
@@ -77,222 +77,118 @@ const CompaniesList = () => {
 	}, [selectedStops, selectedFilterCompany]);
 
 	return (
-		<View style={{
-			paddingHorizontal: 20,
-			marginBottom: 60,
-			flex: 1,
-			backgroundColor: "#fff",
-		}}>
-
-			{/* Header */}
+		<SafeAreaView style={{ flex: 1 }}>
 			<View style={{
-				paddingVertical: 10,
-				paddingHorizontal: 5,
-				borderRadius: 10,
-				marginBottom: 10
-			}}>
-				<Text style={{ fontSize: 20, fontFamily: 'Almarai-Bold' }}>الشركات الخاصة</Text>
-			</View>
-
-			{/* SearchBar */}
-			<View style={{
-				flexDirection: "row-reverse",
-				alignItems: "center",
-				justifyContent: "space-between",
-				marginBottom: 10,
-				zIndex: 100,
+				paddingHorizontal: 20,
+				marginBottom: 60,
+				flex: 1,
+				backgroundColor: "#fff",
 			}}>
 
+				{/* Header */}
 				<View style={{
-					flexDirection: "row",
-					alignItems: "center",
-					backgroundColor: "#f6f6f6",
+					paddingVertical: 10,
+					paddingHorizontal: 5,
 					borderRadius: 10,
-					paddingHorizontal: 10,
-					paddingVertical: 5,
-					flex: 1,
-					marginLeft: 10
+					marginBottom: 10
 				}}>
-					<View style={{ flex: 1 }}>
-						<ScrollView>
-							<TextInput
-
-								placeholder={selectedStops.length > 0 ?
-									"ابحث عن محطة اخرى ..."
-									: "ابحث عن محطة ..."}
-								style={{
-									fontSize: 16,
-									fontFamily: 'Almarai-Light',
-									textAlign: "right",
-									flex: 1,
-									marginVertical: selectedStops.length > 0 ? 10 : 0
-								}}
-								onChangeText={(text) => {
-									setSearchText(text);
-									searchList(text)
-								}}
-								value={searchText}
-								// onBlur={() => setFocus(false)}
-								onFocus={() => setFocus(true)}
-							/>
-							{selectedStops.length > 0 &&
-								<View style={{
-									flexDirection: "row-reverse",
-									flexWrap: "wrap",
-									marginTop: 5
-								}}>
-									{selectedStops.map((stop, index) => {
-										return (
-											<TouchableOpacity key={index}
-												onPress={() => {
-													setSelectedStops(selectedStops.filter((item) => item !== stop));
-												}}
-												style={{
-													backgroundColor: "#fff",
-													borderRadius: 10,
-													paddingHorizontal: 10,
-													paddingVertical: 5,
-													marginRight: 5,
-													marginBottom: 5,
-													flexDirection: "row-reverse",
-												}}>
-												<Text style={{ fontSize: 14, fontFamily: 'Almarai-Light' }}>{stop}</Text>
-												<AntDesign name="close" size={14} color="#656c9e" style={{ marginVertical: 5, marginRight: 10 }} />
-											</TouchableOpacity>
-										);
-									})}
-								</View>
-							}
-						</ScrollView>
-					</View>
-
-					<AntDesign name="search1" size={24} color="#656c9e" style={{ marginVertical: 5, marginLeft: 10 }} />
+					<Text style={{ fontSize: 20, fontFamily: 'Almarai-Bold' }}>الشركات الخاصة</Text>
 				</View>
-				<TouchableOpacity style={{
-					backgroundColor: "#f6f6f6",
-					borderRadius: 10,
-					padding: 10,
-					alignSelf: "flex-start",
-				}}
-					onPress={() => {
-						updateCompaniesList()
-					}}
-				>
-					<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>بحث</Text>
-				</TouchableOpacity>
-				{searchItems.length > 0 && focus &&
-					<View
-						style={{
-							position: "absolute",
-							top: 50,
-							right: 0,
-							left: 0,
-							backgroundColor: "#fff",
-							borderRadius: 10,
-							padding: 10,
-							shadowColor: "#000",
-							shadowOffset: {
-								width: 0,
-								height: 2
-							},
-							shadowOpacity: 0.25,
-							shadowRadius: 3.84,
-							elevation: 5,
-							zIndex: 100,
-							maxHeight: 300,
-						}}>
-						<ScrollView>
-							{searchItems.map((result, index) => {
-								return (
-									<TouchableOpacity key={index} style={{
-										flexDirection: "row-reverse",
-										alignItems: "center",
-										justifyContent: "space-between",
-										backgroundColor: "#f6f6f6",
-										borderRadius: 10,
-										paddingHorizontal: 10,
-										paddingVertical: 5,
-										marginBottom: index == searchItems.length - 1 ? 0 : 10
-									}}
-										onPress={() => {
-											setSelectedStops([...selectedStops, result]);
-											setSearchText("");
-											setSearchItems([]);
-										}}
-									>
-										<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>{result}</Text>
-										<AntDesign name="plus" size={18} color="#656c9e" style={{ marginVertical: 5, marginLeft: 10 }} />
-									</TouchableOpacity>
-								);
-							})}
-						</ScrollView>
-					</View>
-				}
-			</View>
 
-			<View style={{
-				flexDirection: "row-reverse",
-				alignItems: "center",
-				justifyContent: "space-between",
-				marginVertical: 10,
-				zIndex: 80,
-
-			}}>
-				<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>
-					تصفية حسب الشركة
-				</Text>
-
-				<TouchableOpacity style={{
+				{/* SearchBar */}
+				<View style={{
 					flexDirection: "row-reverse",
 					alignItems: "center",
 					justifyContent: "space-between",
-					backgroundColor: "#f6f6f6",
-					borderRadius: 10,
-					paddingHorizontal: 15,
-					paddingVertical: 5,
-					marginBottom: 10
-				}}
-					onPress={() => setDropdownVisible(true)}
+					marginBottom: 10,
+					zIndex: 100,
+				}}>
 
-					ref={dropdownRef}
-					onLayout={() => {
-						dropdownRef.current.measure((x, y, width, height, pageX, pageY) => {
-							setDropdownLayout({ x: pageX, y: pageY, width, height });
-						});
+					<View style={{
+						flexDirection: "row",
+						alignItems: "center",
+						backgroundColor: "#f6f6f6",
+						borderRadius: 10,
+						paddingHorizontal: 10,
+						paddingVertical: 5,
+						flex: 1,
+						marginLeft: 10
+					}}>
+						<View style={{ flex: 1 }}>
+							<ScrollView>
+								<TextInput
+
+									placeholder={selectedStops.length > 0 ?
+										"ابحث عن محطة اخرى ..."
+										: "ابحث عن محطة ..."}
+									style={{
+										fontSize: 16,
+										fontFamily: 'Almarai-Light',
+										textAlign: "right",
+										flex: 1,
+										marginVertical: selectedStops.length > 0 ? 10 : 0
+									}}
+									onChangeText={(text) => {
+										setSearchText(text);
+										searchList(text)
+									}}
+									value={searchText}
+									// onBlur={() => setFocus(false)}
+									onFocus={() => setFocus(true)}
+								/>
+								{selectedStops.length > 0 &&
+									<View style={{
+										flexDirection: "row-reverse",
+										flexWrap: "wrap",
+										marginTop: 5
+									}}>
+										{selectedStops.map((stop, index) => {
+											return (
+												<TouchableOpacity key={index}
+													onPress={() => {
+														setSelectedStops(selectedStops.filter((item) => item !== stop));
+													}}
+													style={{
+														backgroundColor: "#fff",
+														borderRadius: 10,
+														paddingHorizontal: 10,
+														paddingVertical: 5,
+														marginRight: 5,
+														marginBottom: 5,
+														flexDirection: "row-reverse",
+													}}>
+													<Text style={{ fontSize: 14, fontFamily: 'Almarai-Light' }}>{stop}</Text>
+													<AntDesign name="close" size={14} color="#656c9e" style={{ marginVertical: 5, marginRight: 10 }} />
+												</TouchableOpacity>
+											);
+										})}
+									</View>
+								}
+							</ScrollView>
+						</View>
+
+						<AntDesign name="search1" size={24} color="#656c9e" style={{ marginVertical: 5, marginLeft: 10 }} />
+					</View>
+					<TouchableOpacity style={{
+						backgroundColor: "#f6f6f6",
+						borderRadius: 10,
+						padding: 10,
+						alignSelf: "flex-start",
 					}}
-				>
-					<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light', marginLeft: 25 }}>
-						{selectedFilterCompany ? selectedFilterCompany : "جميع الشركات"}
-					</Text>
-					<AntDesign name="down" size={18} color="#656c9e" style={{
-						marginVertical: 5,
-						transform: [{ rotate: !dropdownVisible ? '0deg' : '-180deg' }]
-					}} />
-				</TouchableOpacity>
-
-
-				{dropdownLayout && (
-					<Modal
-						animationType="fade"
-						transparent={true}
-						visible={dropdownVisible}
-						onRequestClose={() => {
-							setDropdownVisible(false);
+						onPress={() => {
+							updateCompaniesList()
 						}}
 					>
-						<TouchableOpacity
+						<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>بحث</Text>
+					</TouchableOpacity>
+					{searchItems.length > 0 && focus &&
+						<View
 							style={{
-								flex: 1,
-								justifyContent: 'center',
-								alignItems: 'center',
-								// backgroundColor: 'rgba(0,0,0,0.5)',
-							}}
-							onPress={() => setDropdownVisible(false)}
-						>
-							<View style={{
-								position: 'absolute',
+								position: "absolute",
+								top: 50,
+								right: 0,
+								left: 0,
 								backgroundColor: "#fff",
-								minWidth: 160,
 								borderRadius: 10,
 								padding: 10,
 								shadowColor: "#000",
@@ -305,13 +201,11 @@ const CompaniesList = () => {
 								elevation: 5,
 								zIndex: 100,
 								maxHeight: 300,
-								top: dropdownLayout.y + dropdownLayout.height - 40,
-								left: dropdownLayout.x,
 							}}>
-								<ScrollView>
-
-									<TouchableOpacity
-										style={{
+							<ScrollView>
+								{searchItems.map((result, index) => {
+									return (
+										<TouchableOpacity key={index} style={{
 											flexDirection: "row-reverse",
 											alignItems: "center",
 											justifyContent: "space-between",
@@ -319,19 +213,106 @@ const CompaniesList = () => {
 											borderRadius: 10,
 											paddingHorizontal: 10,
 											paddingVertical: 5,
-											marginBottom: 10
+											marginBottom: index == searchItems.length - 1 ? 0 : 10
 										}}
-										onPress={() => {
-											setSelectedFilterCompany(null);
-											setDropdownVisible(false);
-										}}
-									>
-										<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>جميع الشركات</Text>
-									</TouchableOpacity>
+											onPress={() => {
+												setSelectedStops([...selectedStops, result]);
+												setSearchText("");
+												setSearchItems([]);
+											}}
+										>
+											<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>{result}</Text>
+											<AntDesign name="plus" size={18} color="#656c9e" style={{ marginVertical: 5, marginLeft: 10 }} />
+										</TouchableOpacity>
+									);
+								})}
+							</ScrollView>
+						</View>
+					}
+				</View>
 
-									{companiesList?.transportationCompanies.map((company, index) => {
-										return (
-											<TouchableOpacity key={index} style={{
+				<View style={{
+					flexDirection: "row-reverse",
+					alignItems: "center",
+					justifyContent: "space-between",
+					marginVertical: 10,
+					zIndex: 80,
+
+				}}>
+					<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>
+						تصفية حسب الشركة
+					</Text>
+
+					<TouchableOpacity style={{
+						flexDirection: "row-reverse",
+						alignItems: "center",
+						justifyContent: "space-between",
+						backgroundColor: "#f6f6f6",
+						borderRadius: 10,
+						paddingHorizontal: 15,
+						paddingVertical: 5,
+						marginBottom: 10
+					}}
+						onPress={() => setDropdownVisible(true)}
+
+						ref={dropdownRef}
+						onLayout={() => {
+							dropdownRef.current.measure((x, y, width, height, pageX, pageY) => {
+								setDropdownLayout({ x: pageX, y: pageY, width, height });
+							});
+						}}
+					>
+						<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light', marginLeft: 25 }}>
+							{selectedFilterCompany ? selectedFilterCompany : "جميع الشركات"}
+						</Text>
+						<AntDesign name="down" size={18} color="#656c9e" style={{
+							marginVertical: 5,
+							transform: [{ rotate: !dropdownVisible ? '0deg' : '-180deg' }]
+						}} />
+					</TouchableOpacity>
+
+
+					{dropdownLayout && (
+						<Modal
+							animationType="fade"
+							transparent={true}
+							visible={dropdownVisible}
+							onRequestClose={() => {
+								setDropdownVisible(false);
+							}}
+						>
+							<TouchableOpacity
+								style={{
+									flex: 1,
+									justifyContent: 'center',
+									alignItems: 'center',
+									// backgroundColor: 'rgba(0,0,0,0.5)',
+								}}
+								onPress={() => setDropdownVisible(false)}
+							>
+								<View style={{
+									position: 'absolute',
+									backgroundColor: "#fff",
+									minWidth: 160,
+									borderRadius: 10,
+									padding: 10,
+									shadowColor: "#000",
+									shadowOffset: {
+										width: 0,
+										height: 2
+									},
+									shadowOpacity: 0.25,
+									shadowRadius: 3.84,
+									elevation: 5,
+									zIndex: 100,
+									maxHeight: 300,
+									top: dropdownLayout.y + dropdownLayout.height - 40,
+									left: dropdownLayout.x,
+								}}>
+									<ScrollView>
+
+										<TouchableOpacity
+											style={{
 												flexDirection: "row-reverse",
 												alignItems: "center",
 												justifyContent: "space-between",
@@ -339,43 +320,64 @@ const CompaniesList = () => {
 												borderRadius: 10,
 												paddingHorizontal: 10,
 												paddingVertical: 5,
-												marginBottom: index == companiesList?.transportationCompanies.length - 1 ? 0 : 10
+												marginBottom: 10
 											}}
-												onPress={() => {
-													setSelectedFilterCompany(company.name);
-													setDropdownVisible(false);
+											onPress={() => {
+												setSelectedFilterCompany(null);
+												setDropdownVisible(false);
+											}}
+										>
+											<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>جميع الشركات</Text>
+										</TouchableOpacity>
+
+										{companiesList?.transportationCompanies.map((company, index) => {
+											return (
+												<TouchableOpacity key={index} style={{
+													flexDirection: "row-reverse",
+													alignItems: "center",
+													justifyContent: "space-between",
+													backgroundColor: "#f6f6f6",
+													borderRadius: 10,
+													paddingHorizontal: 10,
+													paddingVertical: 5,
+													marginBottom: index == companiesList?.transportationCompanies.length - 1 ? 0 : 10
 												}}
-											>
-												<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>{company.name}</Text>
-											</TouchableOpacity>
-										);
-									})}
+													onPress={() => {
+														setSelectedFilterCompany(company.name);
+														setDropdownVisible(false);
+													}}
+												>
+													<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>{company.name}</Text>
+												</TouchableOpacity>
+											);
+										})}
 
-								</ScrollView>
-							</View>
-						</TouchableOpacity>
-					</Modal>
-				)}
-			</View>
-
-			{data?.transportationCompanies.length > 0 ?
-				<FlatList data={data?.transportationCompanies}
-					renderItem={({ item, index }) => <CompanyCard company={item} index={index} isSearch={selectedStops.length > 0 ? true : false} />}
-					keyExtractor={(item, index) => index.toString()}
-				/>
-				:
-				<View style={{
-					flex: 1,
-					alignItems: "center",
-					marginTop: 130
-				}}>
-					<AntDesign name="frowno" size={50} color="#656c9e" />
-					<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light', textAlign: "center", marginTop: 20 }}>لا توجد نتائج</Text>
-					<Text style={{ fontSize: 14, fontFamily: 'Almarai-Light', textAlign: "center", marginTop: 10, marginHorizontal: 30 }}>جرب تغيير الشركة اذا كانت محددة او البحث عن محطة اخرى.</Text>
+									</ScrollView>
+								</View>
+							</TouchableOpacity>
+						</Modal>
+					)}
 				</View>
 
-			}
-		</View>
+				{data?.transportationCompanies.length > 0 ?
+					<FlatList data={data?.transportationCompanies}
+						renderItem={({ item, index }) => <CompanyCard company={item} index={index} isSearch={selectedStops.length > 0 ? true : false} />}
+						keyExtractor={(item, index) => index.toString()}
+					/>
+					:
+					<View style={{
+						flex: 1,
+						alignItems: "center",
+						marginTop: 130
+					}}>
+						<AntDesign name="frowno" size={50} color="#656c9e" />
+						<Text style={{ fontSize: 16, fontFamily: 'Almarai-Light', textAlign: "center", marginTop: 20 }}>لا توجد نتائج</Text>
+						<Text style={{ fontSize: 14, fontFamily: 'Almarai-Light', textAlign: "center", marginTop: 10, marginHorizontal: 30 }}>جرب تغيير الشركة اذا كانت محددة او البحث عن محطة اخرى.</Text>
+					</View>
+
+				}
+			</View>
+		</SafeAreaView>
 	);
 }
 
--- app/screens/RouteDetails.js
+++ app/screens/RouteDetails.js
@@ -6,6 +6,7 @@ import Entypo from 'react-native-vector-icons/Entypo';
 import Feather from 'react-native-vector-icons/Feather';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const RouteDetails = ({ route }) => {
 	const navigation = useNavigation();
@@ -14,169 +15,171 @@ const RouteDetails = ({ route }) => {
 	const [contactsCollapsed, setContactsCollapsed] = React.useState(true);
 
 	return (
-		<View style={{
-			paddingHorizontal: 20,
-			flex: 1,
-			backgroundColor: "#fff",
-		}}>
-			<TouchableOpacity
-				onPress={() => navigation.goBack()}
-				style={{
-					borderRadius: 10,
-					backgroundColor: "#f6f6f6",
-					flexDirection: "column",
-					width: 50,
-					height: 50,
-					alignItems: "center",
-					justifyContent: "center",
-					position: "absolute",
-					top: 50,
-					left: 20,
-				}}>
-				<Octicons name="chevron-left" size={30} color="#000" />
-			</TouchableOpacity>
-
+		<SafeAreaView style={{ flex: 1 }}>
 			<View style={{
-				marginTop: 30,
-				borderRadius: 10,
-				flexDirection: "column",
-				alignItems: "center"
+				paddingHorizontal: 20,
+				flex: 1,
+				backgroundColor: "#fff",
 			}}>
-				<Image
-					source={{ uri: companyDetails.logo }}
+				<TouchableOpacity
+					onPress={() => navigation.goBack()}
 					style={{
-						width: 80,
-						height: 80,
-						borderRadius: 15,
-						backgroundColor: '#f5f5f5',
-					}} resizeMode="contain" />
-
-				<Octicons name="organization" color="#656c9e"
-					size={43}
-					style={{ marginVertical: 18, position: 'absolute', }}
-				/>
-				<View style={{ marginVertical: 10 }}>
-					<Text style={{ fontSize: 24, fontFamily: 'Almarai-Bold' }}>{companyDetails.name}</Text>
-				</View>
-			</View>
-
-			<TouchableOpacity
-				onPress={() => setContactsCollapsed(!contactsCollapsed)}
-				style={{
-					padding: 10,
-					marginBottom: 10,
-					borderRadius: 10,
-					backgroundColor: "#f6f6f6",
-					flexDirection: "row-reverse",
-					justifyContent: "space-between",
-					alignItems: "center",
-					width: "100%"
-				}}>
-				<View style={{
-					flexDirection: "row-reverse",
-					alignItems: "center",
-				}}>
-					<AntDesign name="contacts" size={20} color="#000" />
-					<View style={{
-						marginHorizontal: 10,
+						borderRadius: 10,
+						backgroundColor: "#f6f6f6",
+						flexDirection: "column",
+						width: 50,
+						height: 50,
+						alignItems: "center",
+						justifyContent: "center",
+						position: "absolute",
+						top: 50,
+						left: 20,
 					}}>
-						<Text style={{ fontSize: 17, fontFamily: 'Almarai-Bold' }}>جهات الاتصال</Text>
+					<Octicons name="chevron-left" size={30} color="#000" />
+				</TouchableOpacity>
+
+				<View style={{
+					marginTop: 30,
+					borderRadius: 10,
+					flexDirection: "column",
+					alignItems: "center"
+				}}>
+					<Image
+						source={{ uri: companyDetails.logo }}
+						style={{
+							width: 80,
+							height: 80,
+							borderRadius: 15,
+							backgroundColor: '#f5f5f5',
+						}} resizeMode="contain" />
+
+					<Octicons name="organization" color="#656c9e"
+						size={43}
+						style={{ marginVertical: 18, position: 'absolute', }}
+					/>
+					<View style={{ marginVertical: 10 }}>
+						<Text style={{ fontSize: 24, fontFamily: 'Almarai-Bold' }}>{companyDetails.name}</Text>
 					</View>
 				</View>
-				<Entypo name="chevron-thin-down" size={15} color="#656c9e" style={{
-					marginHorizontal: 5,
-					transform: [{ rotate: contactsCollapsed ? '0deg' : '-180deg' }]
-				}} />
-			</TouchableOpacity>
 
-			{!contactsCollapsed && (
-				<View style={{
-					padding: 10,
-					marginBottom: 10,
-					borderRadius: 10,
-					backgroundColor: "#f6f6f6",
-					flexDirection: "column",
-					alignItems: "center",
-					width: "100%"
-				}}>
-					{companyDetails.contacts.map((contact, index) => {
-						return (
-							<View style={{
-								flexDirection: "row-reverse", alignItems: "center", justifyContent: "space-between", width: "100%", marginVertical: 5,
-							}}>
-								<Text key={index} style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>{contact.name} - {contact.phone}</Text>
-								<TouchableOpacity style={{
-									padding: 5,
-									borderRadius: 8,
-									backgroundColor: "#fff",
-									flexDirection: "column",
-									alignItems: "center",
-									justifyContent: "center",
-									marginHorizontal: 5
-								}}
-									onPress={() => {
-										Linking.openURL(`tel:${contact.phone}`);
-									}}
-								>
-									<Feather name="phone" size={20} color="#000" />
-								</TouchableOpacity>
-							</View>
-						);
-					})}
-				</View>
-			)}
-
-			{/* route details */}
-			<Text style={{
-				fontSize: 20,
-				fontFamily: 'Almarai-Bold',
-				marginBottom: 20,
-			}}>
-				تفاصيل خط {routeDetails.routeName}
-			</Text>
-			<FlatList
-				style={{
-					marginVertical: 10,
-				}}
-				data={routeDetails.busStops}
-				renderItem={({ item, index }) => (
-					<View style={{
-						flexDirection: "column",
-						alignItems: "flex-end",
-						width: "100%",
+				<TouchableOpacity
+					onPress={() => setContactsCollapsed(!contactsCollapsed)}
+					style={{
+						padding: 10,
+						marginBottom: 10,
+						borderRadius: 10,
+						backgroundColor: "#f6f6f6",
+						flexDirection: "row-reverse",
+						justifyContent: "space-between",
+						alignItems: "center",
+						width: "100%"
 					}}>
+					<View style={{
+						flexDirection: "row-reverse",
+						alignItems: "center",
+					}}>
+						<AntDesign name="contacts" size={20} color="#000" />
 						<View style={{
-							padding: 10,
-							margin: 5,
-							borderRadius: 10,
-							backgroundColor: "#f6f6f6",
-							flexDirection: "row-reverse",
-							justifyContent: "space-between",
-							alignItems: "center",
+							marginHorizontal: 10,
 						}}>
-							<View style={{
-								flexDirection: "row-reverse",
-								alignItems: "center",
-							}}>
-								<Octicons name="location" size={20} color="#000" />
-								<View style={{
-									marginHorizontal: 10,
-								}}>
-									<Text style={{ fontSize: 14, fontFamily: 'Almarai-Bold' }}>{item.name}</Text>
-								</View>
-							</View>
+							<Text style={{ fontSize: 17, fontFamily: 'Almarai-Bold' }}>جهات الاتصال</Text>
 						</View>
-						{routeDetails.busStops?.length - 1 !== index
-							&&
-							<MaterialIcons name="keyboard-double-arrow-down" size={30} color="#000" style={{
-								marginHorizontal: 30
-							}} />
-						}
+					</View>
+					<Entypo name="chevron-thin-down" size={15} color="#656c9e" style={{
+						marginHorizontal: 5,
+						transform: [{ rotate: contactsCollapsed ? '0deg' : '-180deg' }]
+					}} />
+				</TouchableOpacity>
+
+				{!contactsCollapsed && (
+					<View style={{
+						padding: 10,
+						marginBottom: 10,
+						borderRadius: 10,
+						backgroundColor: "#f6f6f6",
+						flexDirection: "column",
+						alignItems: "center",
+						width: "100%"
+					}}>
+						{companyDetails.contacts.map((contact, index) => {
+							return (
+								<View style={{
+									flexDirection: "row-reverse", alignItems: "center", justifyContent: "space-between", width: "100%", marginVertical: 5,
+								}}>
+									<Text key={index} style={{ fontSize: 16, fontFamily: 'Almarai-Light' }}>{contact.name} - {contact.phone}</Text>
+									<TouchableOpacity style={{
+										padding: 5,
+										borderRadius: 8,
+										backgroundColor: "#fff",
+										flexDirection: "column",
+										alignItems: "center",
+										justifyContent: "center",
+										marginHorizontal: 5
+									}}
+										onPress={() => {
+											Linking.openURL(`tel:${contact.phone}`);
+										}}
+									>
+										<Feather name="phone" size={20} color="#000" />
+									</TouchableOpacity>
+								</View>
+							);
+						})}
 					</View>
 				)}
-				keyExtractor={item => item.id}
-			/>
-		</View>
+
+				{/* route details */}
+				<Text style={{
+					fontSize: 20,
+					fontFamily: 'Almarai-Bold',
+					marginBottom: 20,
+				}}>
+					تفاصيل خط {routeDetails.routeName}
+				</Text>
+				<FlatList
+					style={{
+						marginVertical: 10,
+					}}
+					data={routeDetails.busStops}
+					renderItem={({ item, index }) => (
+						<View style={{
+							flexDirection: "column",
+							alignItems: "flex-end",
+							width: "100%",
+						}}>
+							<View style={{
+								padding: 10,
+								margin: 5,
+								borderRadius: 10,
+								backgroundColor: "#f6f6f6",
+								flexDirection: "row-reverse",
+								justifyContent: "space-between",
+								alignItems: "center",
+							}}>
+								<View style={{
+									flexDirection: "row-reverse",
+									alignItems: "center",
+								}}>
+									<Octicons name="location" size={20} color="#000" />
+									<View style={{
+										marginHorizontal: 10,
+									}}>
+										<Text style={{ fontSize: 14, fontFamily: 'Almarai-Bold' }}>{item.name}</Text>
+									</View>
+								</View>
+							</View>
+							{routeDetails.busStops?.length - 1 !== index
+								&&
+								<MaterialIcons name="keyboard-double-arrow-down" size={30} color="#000" style={{
+									marginHorizontal: 30
+								}} />
+							}
+						</View>
+					)}
+					keyExtractor={item => item.id}
+				/>
+			</View>
+		</SafeAreaView>
 	);
 }
 
